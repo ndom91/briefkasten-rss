f9d1c9147d1d2badef980d9d137322dc1e47dc96
ndom91 <yo@ndo.dev>
2024-01-11 16:53:47 +0100

    fix: simplify route handler and use action(s) enum

--- src/routes/feed.ts
+++ src/routes/feed.ts
@@ -1,33 +1,26 @@
 import type { FastifyInstance, FastifyRequest, FastifyReply, FastifyPluginOptions } from 'fastify'
+import { actions } from "@lib/constants"
+import { postFeedSchema } from "@routes/route-schemas"
 
 export async function routes(fastify: FastifyInstance, _options: FastifyPluginOptions) {
-  const feedBodySchema = {
-    type: 'object',
-    required: ['feedUrl', 'userId'],
-    properties: {
-      feedUrl: { type: 'string' },
-      userId: { type: 'string' },
-    },
-  }
-
-  const schema = {
-    body: feedBodySchema,
-  }
-
   fastify.get("/feed", async (_request: FastifyRequest, _reply: FastifyReply) => {
     return fastify.queue.getQueue()
   });
 
-  fastify.post("/feed", { schema }, async (request: FastifyRequest, _reply: FastifyReply) => {
-    // @ts-expect-error
-    const { feedUrl, userId } = request.body;
+  fastify.post("/feed", { schema: postFeedSchema }, async (request: FastifyRequest, _reply: FastifyReply) => {
     try {
+      // @ts-expect-error
+      const { feedUrl, userId } = request.body;
       if (!feedUrl || !userId) {
-        throw new Error("feedUrl required");
+        throw new Error("feedUrl and userId required");
       }
       await fastify.queue.push({
-        feedUrl,
-        userId,
+        // @ts-expect-error
+        action: actions.ADD_FEED,
+        data: {
+          feedUrl,
+          userId,
+        }
       });
       return { type: "success" };
     } catch (error) {
