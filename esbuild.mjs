1da5fc06aea6816821997af8b7fdaae44376bf34
ndom91 <yo@ndo.dev>
2024-01-15 19:41:37 +0100

    fix: prod build :pray:

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,18 +1,24 @@
-import * as esbuild from "esbuild";
-import glob from 'tiny-glob';
-
-(async function() {
-  const entryPoints = await glob('src/**/*.ts')
+import * as esbuild from "esbuild"
+import glob from "tiny-glob"
+;(async function () {
+  const entryPoints = await glob("src/**/*.ts")
 
   await esbuild.build({
     entryPoints,
-    // entryPoints: ['src/index.ts'],
-    logLevel: 'info',
-    outdir: 'build',
+    // sourcemap: true,
+    // entryPoints: ["src/index.ts"],
+    logLevel: "info",
+    outdir: "build",
     bundle: true,
-    minify: true,
-    platform: 'node',
-    format: 'cjs',
-    sourcemap: true,
+    minify: false,
+    platform: "node",
+    target: "esnext",
+    format: "esm",
+    banner: {
+      js: `import { createRequire } from 'module';const require = createRequire(import.meta.url);const __filename = (await import('node:url')).fileURLToPath(import.meta.url);const __dirname = (await import('node:path')).dirname(__filename);`,
+    },
+    outExtension: {
+      ".js": ".mjs",
+    },
   })
 })()
